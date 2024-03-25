44f8ff2140bae3d552f68f275a417d3185be03f4
Juhyung Nam <njh1019@gmail.com>
2024-03-25 17:10:32 +0900

    Node 생성자

---
+++ Node.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class Node {
+    int data; //데이터를 저장하는 필드
+    Node next; // 다음 노드 주소를 저장하는 필드
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+
+
+}
+
+
